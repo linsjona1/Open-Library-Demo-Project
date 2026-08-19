5594a71b4ec075f7016bf4c07256a151ca3bfe32
jonathan <jonathanehionye@gmail.com>
2026-08-19 18:07:51 +0100

    Setup

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+// import productRouter from  "./Routes/productRoutes"
+import dotenv from "dotenv";
+
+
+dotenv.config({ path: "./src/.env" });
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+
+
+
+// app.use("/products", productRouter)
+
+app.get('/health', (req, res) => {
+    res.json({ status: 'ok', timestamp: new Date().toISOString() });
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
+
